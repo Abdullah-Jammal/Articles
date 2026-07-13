8ff95dfab614f3ade61600b5065e4259def0af1d
Abdullah Jamal <abdullah.jamal@dtc.iq>
2026-07-13 11:21:51 +0300

    DI

--- Services/Submission/Submission.API/Program.cs
+++ Services/Submission/Submission.API/Program.cs
@@ -1,7 +1,30 @@
+using Submission.API;
 using Submission.API.Endpoints;
+using Submission.Application;
+using Submission.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
+
+#region Add services
+builder.Services
+    .AddApiServices(builder.Configuration)
+    .AddApplicationServices(builder.Configuration)
+    .AddPersistenceServices(builder.Configuration);
+#endregion
+
 var app = builder.Build();
+
+#region Use services
+app.UseSwagger()
+    .UseSwaggerUI()
+    .UseRouting();
+app.MapAllEndpoints();
+// todo
+// Create First Migration and Update Database
+if (app.Environment.IsDevelopment())
+{
+}
+#endregion
 
 app.MapAllEndpoints();
 app.Run();
--- Services/Submission/Submission.Persistence/SubmissionDbContext.cs
+++ Services/Submission/Submission.Persistence/SubmissionDbContext.cs
@@ -3,13 +3,14 @@ using Submission.Domain.Entities;
 
 namespace Submission.Persistence;
 
-public class SubmissionDbContext : DbContext
+public class SubmissionDbContext(DbContextOptions<SubmissionDbContext> options) : DbContext(options)
 {
     public virtual DbSet<Article> Articles { get; set; } = null!;
     public virtual DbSet<Journal> Journals { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
     }
 }
