e381536e4821a121314afe4af7493c34c7e5cf4f
Abdullah Jamal <abdullah.jamal@dtc.iq>
2026-08-17 15:14:31 +0300

    Add Auth Service

---
+++ Services/Auth/Auth.API/Features/CreateUser/CreactUserCommand.cs
@@ -0,0 +1,23 @@
+﻿namespace Auth.API.Features.CreateUser;
+
+public class CreactUserCommand
+{
+    public required string Email { get; init; }
+    public required string FirstName { get; init; }
+    public required string LastName { get; init; }
+    public required int Gender { get; init; }
+    public string? Honorific { get; init; }
+    public string? PhoneNumber { get; init; }
+    public string? PictureUrl { get; init; }
+    public string? CompanyName { get; init; }
+    public string? Position { get; init; }
+    public string? Affiliation { get; init; }
+    public required IReadOnlyList<UserRoleDto> UserRoles { get; init; } = new List<UserRoleDto>();
+}
+
+public record UserRoleDto
+(
+    UserRoleType Type,
+    DateTime? StartDate,
+    DateTime? ExpiringDate
+);
