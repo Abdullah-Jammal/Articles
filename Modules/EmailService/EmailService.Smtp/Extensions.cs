9d5de5e1484c1bd59455eaf9f5ffc61089f347c0
Abdullah Jamal <abdullah.jamal@dtc.iq>
2026-09-02 11:32:39 +0300

    send email msg

---
+++ Modules/EmailService/EmailService.Smtp/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using EmailService.Contracts;
+using MimeKit;
+
+namespace EmailService.Smtp;
+
+public static class Extensions
+{
+    public static MailboxAddress ToMailBoxAddress(this EmailAddress emailAddress)
+    {
+        return new MailboxAddress(emailAddress.Name, emailAddress.Address);
+    }
+    public static MimeMessage ToMailKitMessage(this EmailMessage emailMessage)
+    {
+        var message = new MimeMessage();
+        message.Subject = emailMessage.Subject;
+        message.From.Add(emailMessage.From.ToMailBoxAddress());
+        message.To.AddRange(emailMessage.To.Select(r => r.ToMailBoxAddress()).ToList());
+        var bodyBuilder = new BodyBuilder
+        {
+            HtmlBody = emailMessage.Content.Value
+        };
+        message.Body = bodyBuilder.ToMessageBody();
+        return message;
+    }
+}
